ca720b079722386bc3bd8524214f3f35b8647e33
Michael Puehringer <michael.puehringer@datavisyn.io>
2023-03-09 10:40:18 +0100

    Remove error when parsing of clientConfig failed

--- src/base/clientConfig.ts
+++ src/base/clientConfig.ts
@@ -8,8 +8,7 @@ export interface ITDPClientConfig {
  * Loads the client config from '/clientConfig.json' and parses it.
  */
 export async function loadClientConfig<T = any>(): Promise<T | null> {
-  return Ajax.getJSON('/clientConfig.json').catch((e) => {
-    console.error('Error parsing clientConfig.json', e);
+  return Ajax.getJSON('/clientConfig.json').catch(() => {
     return null;
   });
 }
