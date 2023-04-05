eb4e0f162a09286c2e42d87578e498bcda903c47
Daniela <daniela.moitzi@datavisyn.io>
2023-04-05 17:26:02 +0200

    close burger menu on click outside

--- src/app/header/BurgerMenu.tsx
+++ src/app/header/BurgerMenu.tsx
@@ -1,15 +1,17 @@
 import { Burger, Menu } from '@mantine/core';
+import { useClickOutside } from '@mantine/hooks';
 import React from 'react';
 
 export function BurgerMenu({ menu }: { menu: JSX.Element }) {
   const [opened, setOpened] = React.useState(false);
+  const ref = useClickOutside(() => setOpened(false));
   return (
     <Menu shadow="md" opened={opened}>
       <Menu.Target>
-        <Burger onClick={() => setOpened(!opened)} opened={opened} color="#F8F9FA" size="sm" />
+        <Burger onClick={() => setOpened(!opened)} opened={opened} color="#F8F9FA" size="sm" ref={ref} />
       </Menu.Target>
 
-      <Menu.Dropdown mt={0}>{menu}</Menu.Dropdown>
+      <Menu.Dropdown>{menu}</Menu.Dropdown>
     </Menu>
   );
 }
