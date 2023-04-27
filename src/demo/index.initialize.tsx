203c2c98f403a8ae8ab8ff81598656a619c14dd7
Moritz Heckmann <moritz.heckmann@datavisyn.io>
2023-04-27 09:49:13 +0200

    react18 migration

--- src/demo/index.initialize.tsx
+++ src/demo/index.initialize.tsx
@@ -1,15 +1,19 @@
-import { MantineProvider } from '@mantine/core';
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import { MantineProvider } from '@mantine/core';
 import { VisynAppProvider } from '../app/VisynAppProvider';
 import { MainApp } from './MainApp';
 
 // create a new instance of the app
-ReactDOM.render(
-  <VisynAppProvider appName="Demo App">
-    <MantineProvider withNormalizeCSS withCSSVariables>
-      <MainApp />
-    </MantineProvider>
-  </VisynAppProvider>,
-  document.getElementById('main'),
-);
+const container = document.getElementById('main');
+if (container) {
+  createRoot(container).render(
+    <VisynAppProvider appName="Demo App">
+      <MantineProvider withNormalizeCSS withCSSVariables>
+        <MainApp />
+      </MantineProvider>
+    </VisynAppProvider>,
+  );
+} else {
+  console.error('Could not find container element. This might indicate a problem with the app.');
+}
