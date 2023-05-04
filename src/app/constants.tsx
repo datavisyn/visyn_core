c93c09ad13fb2a77644ca86ffa1a9ffec800de1e
Daniela <daniela.moitzi@datavisyn.io>
2023-05-04 16:11:42 +0200

    add balanced gray color palette

--- src/app/constants.tsx
+++ src/app/constants.tsx
@@ -9,7 +9,7 @@ export const DEFAULT_MANTINE_PROVIDER_PROPS: Omit<MantineProviderProps, 'childre
       fontFamily: 'Roboto, sans-serif',
     },
     colors: {
-      dvGray: ['#E9ECEF', '#DEE2E6', '#CED4DA', '#ADB5BD', '#6C757D', '#495057', '#495057', '#343A40', '#212529', '#1A1B1D'],
+      dvGray: ['#E9ECEF', '#DEE2E6', '#C8CED3', '#BCC3C9', '#ACB4BC', '#99A1A9', '#878E95', '#71787E', '#62686F', '#505459'],
       dvPrimary: ['#E6F3FF', '#A8D7FF', '#79BCF5', '#54A5EB', '#3A8DD5', '#337AB7', '#206198', '#1C4F7B', '#053661', '#062C4E'],
       white: ['white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white'],
       dvDrug: ['#FBE3ED', '#F5C7D9', '#EEACC6', '#E78FB3', '#E074A1', '#B42865', '#9E1D58', '#87184D', '#6F1340', '#560D34'],
