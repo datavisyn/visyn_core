8b02936bcbc70e405c2e52c3b37d20965146204f
dvzacharycutler <zachary.cutler@datavisyn.io>
2023-05-31 15:45:43 +0200

    removing comment

--- src/vis/VisSidebarWrapper.tsx
+++ src/vis/VisSidebarWrapper.tsx
@@ -9,7 +9,6 @@ export function VisSidebarWrapper({ children }: { children: ReactNode }) {
   return (
     <Box pt="sm" style={{ height: '100%', boxShadow: '2px 0px 15px 0px lightgray', zIndex: 5 }}>
       <Group spacing={0} style={{ width: '100%', height: '100%' }} noWrap>
-        {/* <Divider orientation="vertical" size="xs" /> */}
         <ScrollArea p={0} w={`${sidebarSize}px`} h="100%">
           <Box pb="xl" style={{ height: '100%', width: `${sidebarSize}px` }}>
             {children}
