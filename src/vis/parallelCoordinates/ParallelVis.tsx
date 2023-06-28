0a81a3138d98652628434872a13b6ac4ebbf8658
dvzacharycutler <zachary.cutler@datavisyn.io>
2023-06-28 12:03:00 +0200

    fixing with new vis setup

--- src/vis/parallelCoordinates/ParallelVis.tsx
+++ src/vis/parallelCoordinates/ParallelVis.tsx
@@ -4,7 +4,7 @@ import * as d3v7 from 'd3v7';
 import { Group, Stack } from '@mantine/core';
 import { useResizeObserver } from '@mantine/hooks';
 import { Text } from '@mantine/core';
-import { IVisConfig, VisColumn, IParallelCoordinatesConfig } from '../interfaces';
+import { IVisConfig, VisColumn, IParallelCoordinatesConfig, ICommonVisProps } from '../interfaces';
 import { useAsync } from '../../hooks';
 import { VisSidebarWrapper } from '../VisSidebarWrapper';
 import { VisSidebarOpenButton } from '../VisSidebarOpenButton';
@@ -20,69 +20,13 @@ const defaultExtensions = {
 };
 
 export function ParallelVis({
-  config,
-  optionsConfig,
-  extensions,
-  columns,
-  setConfig,
-  enableSidebar,
-  setShowSidebar,
-  showSidebar,
-}: {
-  config: IParallelCoordinatesConfig;
-  optionsConfig?: {
-    color?: {
-      enable?: boolean;
-      customComponent?: React.ReactNode;
-    };
-    shape?: {
-      enable?: boolean;
-      customComponent?: React.ReactNode;
-    };
-    filter?: {
-      enable?: boolean;
-      customComponent?: React.ReactNode;
-    };
-  };
-  extensions?: {
-    prePlot?: React.ReactNode;
-    postPlot?: React.ReactNode;
-    preSidebar?: React.ReactNode;
-    postSidebar?: React.ReactNode;
-  };
-  columns: VisColumn[];
-  setConfig: (config: IVisConfig) => void;
-  showSidebar?: boolean;
-  setShowSidebar?(show: boolean): void;
-  enableSidebar?: boolean;
-}) {
-  return (
-    <Group
-      noWrap
-      pl={0}
-      pr={0}
-      sx={{
-        flexGrow: 1,
-        height: '100%',
-        width: '100%',
-        overflow: 'hidden',
-        position: 'relative',
-        // Disable plotly crosshair cursor
-        '.nsewdrag': {
-          cursor: 'pointer !important',
-        },
-      }}
-    >
-      {enableSidebar ? <VisSidebarOpenButton onClick={() => setShowSidebar(!showSidebar)} isOpen={showSidebar} /> : null}
+  externalConfig,
 
-      <Stack spacing={0} sx={{ height: '100%', width: '100%' }}>
-        {config?.numColumnsSelected?.length > 1 ? <ParallelPlot config={config} columns={columns} /> : null}
-      </Stack>
-      {showSidebar ? (
-        <VisSidebarWrapper>
-          <ParallelVisSidebar config={config} optionsConfig={optionsConfig} extensions={extensions} columns={columns} setConfig={setConfig} />
-        </VisSidebarWrapper>
-      ) : null}
-    </Group>
+  columns,
+}: ICommonVisProps<IParallelCoordinatesConfig>) {
+  return (
+    <Stack spacing={0} sx={{ height: '100%', width: '100%' }}>
+      {externalConfig?.numColumnsSelected?.length > 1 ? <ParallelPlot config={externalConfig} columns={columns} /> : null}
+    </Stack>
   );
 }
--- src/vis/provider/utils.ts
+++ src/vis/provider/utils.ts
@@ -12,11 +12,21 @@ import { BarVisSidebar } from '../bar/BarVisSidebar';
 import { HexbinVis } from '../hexbin/HexbinVis';
 import { ESupportedPlotlyVis } from '../interfaces';
 import { createVis, visMap } from './Provider';
+import { ParallelVis } from '../parallelCoordinates/ParallelVis';
+import { ParallelVisSidebar } from '../parallelCoordinates/ParallelVisSidebar';
+import { parallelCoordinatesMergeDefaultConfig } from '../parallelCoordinates/utils';
 
 export function registerAllVis() {
   visMap[ESupportedPlotlyVis.SCATTER] = createVis(ESupportedPlotlyVis.SCATTER, ScatterVis, ScatterVisSidebar, scatterMergeDefaultConfig);
   visMap[ESupportedPlotlyVis.BAR] = createVis(ESupportedPlotlyVis.BAR, BarVis, BarVisSidebar, barMergeDefaultConfig);
   visMap[ESupportedPlotlyVis.VIOLIN] = createVis(ESupportedPlotlyVis.VIOLIN, ViolinVis, ViolinVisSidebar, violinMergeDefaultConfig);
   visMap[ESupportedPlotlyVis.HEXBIN] = createVis(ESupportedPlotlyVis.HEXBIN, HexbinVis, HexbinVisSidebar, hexinbMergeDefaultConfig);
+  visMap[ESupportedPlotlyVis.PARALLEL_COORDINATES] = createVis(
+    ESupportedPlotlyVis.PARALLEL_COORDINATES,
+    ParallelVis,
+    ParallelVisSidebar,
+    parallelCoordinatesMergeDefaultConfig,
+  );
+
   // visMap[ESupportedPlotlyVis.SANKEY] = createVis(ESupportedPlotlyVis.SANKEY, SankeyVis, SankeyVisSidebar);
 }
