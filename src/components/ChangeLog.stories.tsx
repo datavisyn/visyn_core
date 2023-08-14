055224e30033bd6d683feef2a7112df5b1e5c73c
Lili <lili.albrecht@datavisyn.io>
2023-08-14 13:17:10 +0200

    new article added

--- src/components/ChangeLog.stories.tsx
+++ src/components/ChangeLog.stories.tsx
@@ -3,6 +3,7 @@ import { StoryObj } from '@storybook/react';
 import { ChangeLogComponent } from './ChangeLogComponent';
 import readmeBioInSight from './DemoReleaseNotesBioInSight.md';
 import readmeAelixir from './DemoReleaseNotesAelixir.md';
+import readmeBioInSight2 from './DemoReleaseNotesBioInSight2.md';
 
 export default {
   title: 'Example/Ui/ChangeLog',
@@ -18,6 +19,7 @@ export const SecondarySelfmadeChangeLog: Story = {
       data={[
         { title: 'Release version 1.2.2', author: 'username', content: readmeBioInSight, date: new Date(1111, 0, 1), tags: ['Feature', 'Devops'] },
         { title: 'Release v2.1.0', author: 'otherusername', content: readmeAelixir, date: new Date(2023, 7, 1), tags: ['Bug'] },
+        { title: 'Release this week', author: 'myusername', content: readmeBioInSight2, date: new Date(2023, 7, 14), tags: ['Release'] },
       ]}
     />
   ),
