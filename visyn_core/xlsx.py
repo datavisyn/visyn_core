39b12912c6f3b147ebf7828356e126141af5f7d6
dvmartinweigl <martin.weigl@datavisyn.io>
2023-11-05 19:55:44 +0100

    only escape str with len>1

--- visyn_core/xlsx.py
+++ visyn_core/xlsx.py
@@ -105,8 +105,8 @@ def _json2xlsx():
     bold = Font(bold=True)
 
     def _escape(v):
-        if isinstance(v, str) and v and v[0] in ["+", "-", "@", "="]:
-            _log.warning("CSV injection detected: %s", v)
+        if isinstance(v, str) and v and v[0] in ["+", "-", "@", "="] and len(v) > 1:
+            _log.warning("Escaping possible CSV injection: %s", v)
             return f"'{v}"
         return v
 
