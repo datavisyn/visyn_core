426854c35fbd6b577a978dc17bb909c1e4f68ece
oltionchampari <oltion.champari@datavisyn.io>
2024-02-08 14:06:46 +0100

    fix: use style instead of sx

--- src/vis/heatmap/HeatmapGrid.tsx
+++ src/vis/heatmap/HeatmapGrid.tsx
@@ -33,7 +33,7 @@ export function HeatmapGrid({
   }, []);
 
   return (
-    <Stack align="center" justify="center" sx={{ width: '100%', height: '100%' }} p="sm">
+    <Stack align="center" justify="center" style={{ width: '100%', height: '100%' }} p="sm">
       {status === 'pending' && <Loader />}
       {status === 'success' && hasAtLeast2CatCols && (
         <Heatmap
