59947ceda48ada006c446335ba0b6295ea15f013
dvlilialbrecht <lili.albrecht@datavisyn.io>
2024-02-12 13:16:17 +0100

    test

--- src/components/ChangeLog/ChangeLog.stories.tsx
+++ src/components/ChangeLog/ChangeLog.stories.tsx
@@ -1,34 +1,34 @@
-/* eslint-disable no-await-in-loop */
-import * as React from 'react';
-import { StoryObj } from '@storybook/react';
-import { ChangeLogComponent } from './ChangeLogComponent';
-import readmeBioInSight from './DemoReleaseNotesBioInSight.md';
-import readmeAelixir from './DemoReleaseNotesAelixir.md';
-import readmeBioInSight2 from './DemoReleaseNotesBioInSight2.md';
-import readMeDemoFormatter from './DemoFormatter.md';
-import { IChangeLogArticle } from './interfaces';
-import { parseFrontmatter } from './utils';
+// /* eslint-disable no-await-in-loop */
+// import * as React from 'react';
+// import { StoryObj } from '@storybook/react';
+// import { ChangeLogComponent } from './ChangeLogComponent';
+// import readmeBioInSight from './DemoReleaseNotesBioInSight.md';
+// import readmeAelixir from './DemoReleaseNotesAelixir.md';
+// import readmeBioInSight2 from './DemoReleaseNotesBioInSight2.md';
+// import readMeDemoFormatter from './DemoFormatter.md';
+// import { IChangeLogArticle } from './interfaces';
+// import { parseFrontmatter } from './utils';
 
-export default {
-  title: 'Example/Ui/ChangeLog',
-  component: ChangeLogComponent,
-};
+// export default {
+//   title: 'Example/Ui/ChangeLog',
+//   component: ChangeLogComponent,
+// };
 
-type Story = StoryObj<typeof ChangeLogComponent>;
+// type Story = StoryObj<typeof ChangeLogComponent>;
 
-async function fillData(): Promise<IChangeLogArticle[]> {
-  const data: IChangeLogArticle[] = [];
-  for (let index = 0; index <= 10; index++) {
-    data.push(await parseFrontmatter(readMeDemoFormatter));
-    data.push(await parseFrontmatter(readmeBioInSight));
-    data.push(await parseFrontmatter(readmeBioInSight2));
-    data.push(await parseFrontmatter(readmeAelixir));
-  }
-  return data;
-}
+// async function fillData(): Promise<IChangeLogArticle[]> {
+//   const data: IChangeLogArticle[] = [];
+//   for (let index = 0; index <= 10; index++) {
+//     data.push(await parseFrontmatter(readMeDemoFormatter));
+//     data.push(await parseFrontmatter(readmeBioInSight));
+//     data.push(await parseFrontmatter(readmeBioInSight2));
+//     data.push(await parseFrontmatter(readmeAelixir));
+//   }
+//   return data;
+// }
 
-const data = await fillData();
+// const data = await fillData();
 
-export const SecondarySelfmadeChangeLog: Story = {
-  render: () => <ChangeLogComponent data={data} />,
-};
+// export const SecondarySelfmadeChangeLog: Story = {
+//   render: () => <ChangeLogComponent data={data} />,
+// };
