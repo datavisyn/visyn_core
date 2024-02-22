ba1134650dd884f2d63a6e8807831af45f352d83
Daniela <daniela.moitzi@datavisyn.io>
2024-02-22 14:06:10 +0100

    fix logo in header

--- src/app/header/DatavisynLogo.tsx
+++ src/app/header/DatavisynLogo.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Anchor, rem } from '@mantine/core';
+import { Anchor } from '@mantine/core';
 import datavisynLogoWhite from '../../assets/datavisyn_white.svg';
 import datavisynLogoBlack from '../../assets/datavisyn_black.svg';
 import datavisynLogoColor from '../../assets/datavisyn_color.svg';
@@ -7,7 +7,7 @@ import datavisynLogoColor from '../../assets/datavisyn_color.svg';
 export function DatavisynLogo({ color }: { color: 'white' | 'black' | 'color' }) {
   const source = color === 'white' ? datavisynLogoWhite : color === 'black' ? datavisynLogoBlack : datavisynLogoColor;
   return (
-    <Anchor h={rem(23)} href="https://datavisyn.io/" target="_blank">
+    <Anchor display="flex" href="https://datavisyn.io/" target="_blank">
       <img src={source} alt="logo" style={{ height: '24px' }} />
     </Anchor>
   );
