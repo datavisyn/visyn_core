cd58ebeeb553dd3c6a1546c9cf4cf4364b3bda1e
Daniela <daniela.moitzi@datavisyn.io>
2024-03-18 11:30:43 +0100

    test

--- src/@types/mantine-overrides.d.ts
+++ src/@types/mantine-overrides.d.ts
@@ -1,8 +1,7 @@
-import { DefaultMantineColor, MantineColorsTuple } from '@mantine/core';
-
-type ExtendedCustomColors = 'dvPrimary' | 'dvGray' | 'dvGene' | 'dvDisease' | 'dvCellLine' | 'dvTissue' | 'dvDrug' | 'white' | DefaultMantineColor;
-
 declare module '@mantine/core' {
+  import { DefaultMantineColor, MantineColorsTuple } from '@mantine/core';
+
+  type ExtendedCustomColors = 'dvPrimary' | 'dvGray' | 'dvGene' | 'dvDisease' | 'dvCellLine' | 'dvTissue' | 'dvDrug' | 'white' | DefaultMantineColor;
   export interface MantineThemeColorsOverride {
     colors: Record<ExtendedCustomColors, MantineColorsTuple>;
   }
