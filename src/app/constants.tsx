bfbdfb83ce05d42151d2e7492e7993e201b1e1f5
Vanessa Stoiber <52395160+dvvanessastoiber@users.noreply.github.com>
2024-04-11 09:27:10 +0200

    Release 9.2.1 (#255)
    * add new cell line color
    
    * blubb
    
    * chore: prepare next dev release
    
    * update gene and chem structure ic ons
    
    * prepare release 9.2.1
    
    ---------
    
    Co-authored-by: Daniela <daniela.moitzi@datavisyn.io>
    Co-authored-by: Holger Stitz <holger.stitz@datavisyn.io>
    Co-authored-by: dvdanielamoitzi <57343176+dvdanielamoitzi@users.noreply.github.com>
    Co-authored-by: Michael Pühringer <51900829+puehringer@users.noreply.github.com>

--- src/app/constants.tsx
+++ src/app/constants.tsx
@@ -39,7 +39,7 @@ export const DEFAULT_MANTINE_PROVIDER_PROPS: Omit<MantineProviderProps, 'childre
       dvDrug: ['#FBE3ED', '#F5C7D9', '#EEACC6', '#E78FB3', '#E074A1', '#B42865', '#9E1D58', '#87184D', '#6F1340', '#560D34'],
       dvGene: ['#EDF8F1', '#BDEDCA', '#9EE4B2', '#7DD997', '#5DCD7D', '#4BB268', '#439E5E', '#3B894F', '#32743F', '#285F30'],
       dvDisease: ['#FFF0E6', '#FFE1CC', '#FFD2B3', '#FFC399', '#FFB480', '#FF964D', '#E88745', '#CC783D', '#B36A35', '#995D2E'],
-      dvCellLine: ['#EDF8F8', '#C6E8E8', '#9FD7D7', '#78C6C6', '#51B5B5', '#75C4C2', '#5BA3A1', '#427F7E', '#3A6A6A', '#1D4847'],
+      dvCellLine: ['#D9F4F3', '#B5E5E5', '#8FD7D6', '#6FCAC9', '#5BC2C1', '#39A2A1', '#2C9695', '#0B8281', '#087170', '#045B5B'],
       dvTissue: ['#FFF1F0', '#FFE1DD', '#FFD0CA', '#FFBFA7', '#FFAF94', '#E57C73', '#C65A51', '#A63930', '#872617', '#650D00'],
     },
     primaryShade: 5,
