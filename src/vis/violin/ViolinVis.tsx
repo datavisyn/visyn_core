48943a8909950263f0ca635488b09b900ed73fd0
dvmartinweigl <martin.weigl@datavisyn.io>
2024-04-23 17:52:43 +0200

    small refactor

--- src/vis/violin/ViolinVis.tsx
+++ src/vis/violin/ViolinVis.tsx
@@ -1,7 +1,7 @@
 import { Stack } from '@mantine/core';
 import * as d3v7 from 'd3v7';
 import uniqueId from 'lodash/uniqueId';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { useAsync } from '../../hooks';
 import { PlotlyComponent, PlotlyTypes } from '../../plotly';
 import { Plotly } from '../../plotly/full';
@@ -14,7 +14,7 @@ import { createViolinTraces } from './utils';
 export function ViolinVis({ config, columns, scales, dimensions, selectedList, selectedMap, selectionCallback }: ICommonVisProps<IViolinConfig>) {
   const { value: traces, status: traceStatus, error: traceError } = useAsync(createViolinTraces, [columns, config, scales, selectedList, selectedMap]);
 
-  const id = React.useMemo(() => uniqueId('ViolinVis'), []);
+  const id = useMemo(() => uniqueId('ViolinVis'), []);
 
   const [layout, setLayout] = useState<Partial<Plotly.Layout>>(null);
 
@@ -47,14 +47,24 @@ export function ViolinVis({ config, columns, scales, dimensions, selectedList, s
     }
   };
 
+  // NOTE: @dv-usama-ansari: This is an alternative way to delay the resize of plotly plots, but the dependencies of the `useCallback` are unknown if the function is wrapped in lodash `debounce`.
+  // const resizePlotly = useCallback(
+  //   debounce((plotDiv) => {
+  //     Plotly.Plots.resize(plotDiv);
+  //   }),
+  //   [],
+  // );
+
   useEffect(() => {
     const plotDiv = document.getElementById(`plotlyDiv${id}`);
     if (plotDiv) {
-      Plotly.Plots.resize(plotDiv);
+      // NOTE: @dv-usama-ansari: This is a hack to update the plotly plots on resize.
+      //  The `setTimeout` is used to pass the resize function to the next event loop, so that the plotly plots are rendered first.
+      setTimeout(() => Plotly.Plots.resize(plotDiv));
     }
-  }, [id, dimensions]);
+  }, [id, dimensions, traces]);
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (!traces) {
       return;
     }
