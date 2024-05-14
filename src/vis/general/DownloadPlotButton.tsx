eafd6fb55de8ca23a6d792c685001be6c245b4ce
Daniela <daniela.moitzi@datavisyn.io>
2024-05-14 11:39:20 +0200

    add download visualization icon

--- src/vis/general/DownloadPlotButton.tsx
+++ src/vis/general/DownloadPlotButton.tsx
@@ -1,14 +1,16 @@
 import { Tooltip, ActionIcon } from '@mantine/core';
 import * as React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { BaseVisConfig } from '../interfaces';
 import { useCaptureVisScreenshot } from '../useCaptureVisScreenshot';
+import { dvDownloadVisualization } from '../../icons';
 
 export function DownloadPlotButton({ uniquePlotId, config }: { config: BaseVisConfig; uniquePlotId: string }) {
   const [{ isLoading }, captureScreenshot] = useCaptureVisScreenshot(uniquePlotId, config);
   return (
-    <Tooltip label="Download as image" position="top">
+    <Tooltip label="Download visualization as image" position="top" withArrow>
       <ActionIcon color="dvGray" loading={isLoading} variant="subtle" onClick={captureScreenshot}>
-        <i className="fa-solid fa-camera" />
+        <FontAwesomeIcon icon={dvDownloadVisualization} />
       </ActionIcon>
     </Tooltip>
   );
