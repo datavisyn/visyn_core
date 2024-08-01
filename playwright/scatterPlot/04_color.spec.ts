c3b6d23027827044c327b7d7165fce195457a4ee
dvlilialbrecht <lili.albrecht@datavisyn.io>
2024-08-01 14:07:58 +0200

    delay

--- playwright/scatterPlot/04_color.spec.ts
+++ playwright/scatterPlot/04_color.spec.ts
@@ -1,13 +1,12 @@
 import { test, expect, takeSnapshot, ChromaticConfig } from '@chromatic-com/playwright';
 
 test.only('no color selected', async ({ page }, testInfo) => {
-  test.use({ delay: 1000 });
+  // test.use({ delay: 1000 });
   await page.goto('/');
   await expect(page.getByTestId('SingleSelectColor').locator('span[class*="InputPlaceholder-placeholder"]')).toBeVisible();
   await expect(page.getByLabel('Legend')).toBeDisabled();
   await expect(page.locator('g[class="legend"]')).not.toBeVisible();
-  // delay chromatic snapshot for 300 ms
-  await page.waitForTimeout(300);
+  await page.waitForTimeout(1000);
   await takeSnapshot(page, 'Scatter Plot: no color selected', testInfo);
   //await expect(page.locator('div[class="js-plotly-plot"]')).toHaveScreenshot('scatterPlotInitialState.png');
 });
