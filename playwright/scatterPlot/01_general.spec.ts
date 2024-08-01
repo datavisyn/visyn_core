baf6e0f5fea807d286575702604bf12e155a292b
dvlilialbrecht <lili.albrecht@datavisyn.io>
2024-08-01 07:49:38 +0200

    chromatic playwright

--- playwright/scatterPlot/01_general.spec.ts
+++ playwright/scatterPlot/01_general.spec.ts
@@ -1,6 +1,6 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@chromatic-com/playwright';
 
-test('rectangle brush', async ({ page }) => {
+test.only('rectangle brush', async ({ page }) => {
   await page.goto('/');
   await page.getByTestId('BrushOptions').locator('div[class*="SegmentedControl-control"]').first().click();
   const scatterPlot = await page.locator('div[class="js-plotly-plot"]');
@@ -18,7 +18,7 @@ test('rectangle brush', async ({ page }) => {
   await expect(page.locator('div[class="js-plotly-plot"]')).toHaveScreenshot('ScatterPlotRectangeBrush.png');
 });
 
-test('lasso brush', async ({ page }) => {
+test.only('lasso brush', async ({ page }) => {
   await page.goto('/');
   await page.getByTestId('BrushOptions').locator('div[class*="SegmentedControl-control"]').nth(1).click();
   const scatterPlot = await page.locator('div[class="js-plotly-plot"]');
@@ -45,7 +45,7 @@ test('download plot', async ({ page }) => {
   await download.saveAs(`./download-test-results/01-general/${new Date().toISOString}/${download.suggestedFilename()}`);
 });
 
-test('selection in ranking should be visible in plot', async ({ page }) => {
+test.only('selection in ranking should be visible in plot', async ({ page }) => {
   await page.goto('/');
   for (let index = 1; index <= 50; index++) {
     await page.locator('div[class*="lu-renderer-selection"]').nth(index).click();
