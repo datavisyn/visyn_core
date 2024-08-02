5cfa3ead2d5e3263220e281cd96b9d9611040a00
dvlilialbrecht <lili.albrecht@datavisyn.io>
2024-08-02 10:52:24 +0200

    violin plot: change import to chromatic/playwright

--- playwright/violinPlot/03_categoricalColumns.spec.ts
+++ playwright/violinPlot/03_categoricalColumns.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@chromatic-com/playwright';
 import { selectViolinPlot } from '../extensions/selectPlots';
 
 test('no categorical column selected', async ({ page }) => {
