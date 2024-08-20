50e333d999b35cd2a788759018bc1c2d30e8f778
dvlilialbrecht <lili.albrecht@datavisyn.io>
2024-08-20 12:11:55 +0200

    change waitForTimeout

--- playwright/scatterPlot/05_shape.spec.ts
+++ playwright/scatterPlot/05_shape.spec.ts
@@ -5,11 +5,11 @@ test('no shape selected', async ({ page }) => {
   await expect(page.getByTestId('SingleSelectShape').locator('span[class*="InputPlaceholder-placeholder"]').last()).toBeVisible();
 });
 
-test('shape selected', async ({ page }) => {
+test.only('shape selected', async ({ page }) => {
   await page.goto('/');
   await page.getByTestId('SingleSelectShape').click();
   await page.getByRole('option', { name: 'Breast Surgery Type Sparse' }).click();
   await expect(page.getByLabel('Legend')).not.toBeDisabled();
   await expect(page.locator('g[class="legend"]')).toBeVisible();
-  await page.waitForTimeout(3000);
+  await page.waitForTimeout(10000);
 });
