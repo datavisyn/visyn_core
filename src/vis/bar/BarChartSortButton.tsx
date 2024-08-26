9e8601ad7b67e9bd9f58eaa609971cdf38563184
Usama Ansari <usama.ansari@datavisyn.io>
2024-08-26 16:35:05 +0200

    fix: add null checks for sort button

--- src/vis/bar/BarChartSortButton.tsx
+++ src/vis/bar/BarChartSortButton.tsx
@@ -1,64 +1,77 @@
-import { faQuestion } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { ActionIcon, Tooltip } from '@mantine/core';
 import React, { useMemo } from 'react';
-import { dvSort, dvSortAsc, dvSortDesc } from '../../icons';
-import { EBarDirection, EBarSortState, IBarConfig } from './interfaces';
-import { VIS_LABEL_COLOR } from '../general';
-import { selectionColorDark } from '../../utils';
+import { dvSort, dvSortAsc, dvSortDesc } from 'visyn_core/icons';
+import { selectionColorDark } from 'visyn_core/utils';
+import { EBarDirection, EBarSortState, IBarConfig } from 'visyn_core/vis';
 
 export function BarChartSortButton({ config, setConfig }: { config: IBarConfig; setConfig: (c: IBarConfig) => void }) {
   const { tooltipLabel, icon, color, nextSortState } = useMemo(() => {
     const fallbackSortState = { x: EBarSortState.NONE, y: EBarSortState.NONE };
-    if (config.direction === EBarDirection.HORIZONTAL) {
-      switch (config.sortState.x) {
-        case EBarSortState.NONE:
-          return {
-            tooltipLabel: `Sort ${config.aggregateType} in ascending order`,
-            icon: dvSort,
-            color: VIS_LABEL_COLOR,
-            nextSortState: { ...(config.sortState ?? fallbackSortState), x: EBarSortState.ASCENDING },
-          };
-        case EBarSortState.ASCENDING:
-          return {
-            tooltipLabel: `Sort ${config.aggregateType} in descending order`,
-            icon: dvSortAsc,
-            color: selectionColorDark,
-            nextSortState: { ...(config.sortState ?? fallbackSortState), x: EBarSortState.DESCENDING },
-          };
-        case EBarSortState.DESCENDING:
-          return { tooltipLabel: `Remove sorting from ${config.aggregateType}`, icon: dvSortDesc, color: selectionColorDark, nextSortState: fallbackSortState };
-        default:
-          return { tooltipLabel: '', icon: faQuestion, nextSortState: fallbackSortState };
+    if (config.sortState) {
+      if (config.direction === EBarDirection.HORIZONTAL) {
+        switch (config.sortState.x) {
+          case EBarSortState.ASCENDING:
+            return {
+              tooltipLabel: `Sort ${config.aggregateType} in descending order`,
+              icon: dvSortAsc,
+              color: selectionColorDark,
+              nextSortState: { ...(config.sortState ?? fallbackSortState), x: EBarSortState.DESCENDING },
+            };
+          case EBarSortState.DESCENDING:
+            return {
+              tooltipLabel: `Remove sorting from ${config.aggregateType}`,
+              icon: dvSortDesc,
+              color: selectionColorDark,
+              nextSortState: fallbackSortState,
+            };
+          case EBarSortState.NONE:
+          default:
+            return {
+              tooltipLabel: `Sort ${config.aggregateType} in ascending order`,
+              icon: dvSort,
+              color: 'dark',
+              nextSortState: { ...(config.sortState ?? fallbackSortState), x: EBarSortState.ASCENDING },
+            };
+        }
+      }
+      if (config.direction === EBarDirection.VERTICAL) {
+        switch (config.sortState.y) {
+          case EBarSortState.ASCENDING:
+            return {
+              tooltipLabel: `Sort ${config.aggregateType} in descending order`,
+              icon: dvSortAsc,
+              color: selectionColorDark,
+              nextSortState: { ...(config.sortState ?? fallbackSortState), y: EBarSortState.DESCENDING },
+            };
+          case EBarSortState.DESCENDING:
+            return {
+              tooltipLabel: `Remove sorting from ${config.aggregateType}`,
+              icon: dvSortDesc,
+              color: selectionColorDark,
+              nextSortState: fallbackSortState,
+            };
+          case EBarSortState.NONE:
+          default:
+            return {
+              tooltipLabel: `Sort ${config.aggregateType} in ascending order`,
+              icon: dvSort,
+              color: 'dark',
+              nextSortState: { ...(config.sortState ?? fallbackSortState), y: EBarSortState.ASCENDING },
+            };
+        }
       }
     }
-    if (config.direction === EBarDirection.VERTICAL) {
-      switch (config.sortState.y) {
-        case EBarSortState.NONE:
-          return {
-            tooltipLabel: `Sort ${config.aggregateType} in ascending order`,
-            icon: dvSort,
-            color: VIS_LABEL_COLOR,
-            nextSortState: { ...(config.sortState ?? fallbackSortState), y: EBarSortState.ASCENDING },
-          };
-        case EBarSortState.ASCENDING:
-          return {
-            tooltipLabel: `Sort ${config.aggregateType} in descending order`,
-            icon: dvSortAsc,
-            color: selectionColorDark,
-            nextSortState: { ...(config.sortState ?? fallbackSortState), y: EBarSortState.DESCENDING },
-          };
-        case EBarSortState.DESCENDING:
-          return { tooltipLabel: `Remove sorting from ${config.aggregateType}`, icon: dvSortDesc, color: selectionColorDark, nextSortState: fallbackSortState };
-        default:
-          return { tooltipLabel: '', icon: faQuestion, nextSortState: fallbackSortState };
-      }
-    }
-    return { tooltipLabel: '' };
+    return {
+      tooltipLabel: `Sort ${config.aggregateType} in ascending order`,
+      icon: dvSort,
+      color: 'dark',
+      nextSortState: { ...(config.sortState ?? fallbackSortState), y: EBarSortState.ASCENDING },
+    };
   }, [config.aggregateType, config.direction, config.sortState]);
 
   return (
-    <Tooltip label={tooltipLabel} position="top" withArrow>
+    <Tooltip label={tooltipLabel} disabled={!tooltipLabel} position="top" withArrow>
       <ActionIcon
         c={color}
         variant="subtle"
