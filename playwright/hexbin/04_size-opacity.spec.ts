50850865eb7258cb47bb2f5502fc87810f7dcf45
dvlilialbrecht <lili.albrecht@datavisyn.io>
2024-09-05 13:24:16 +0200

    hexbin no opacity timeout

--- playwright/hexbin/04_size-opacity.spec.ts
+++ playwright/hexbin/04_size-opacity.spec.ts
@@ -33,6 +33,5 @@ test('size scale', async ({ page }) => {
 
 test('no opacity', async ({ page }) => {
   await selectHexbin(page);
-  await page.locator('label').filter({ hasText: 'Opacity scale' }).locator('span').first().click();
-  await page.waitForTimeout(2000);
+  await page.locator('label').filter({ hasText: 'Opacity scale' }).locator('span').first().click({ timeout: 10000 });
 });
