2859229e03513a051cbe00acb2455792bbb2d9ef
Moritz Heckmann <moritz.heckmann@datavisyn.io>
2024-10-15 15:50:48 +0200

    style optional

--- src/vis/general/InvalidCols.tsx
+++ src/vis/general/InvalidCols.tsx
@@ -1,7 +1,7 @@
 import { Alert, Center, Stack } from '@mantine/core';
 import * as React from 'react';
 
-export function InvalidCols({ headerMessage, bodyMessage, style }: { headerMessage: string; bodyMessage: string; style: React.CSSProperties }) {
+export function InvalidCols({ headerMessage, bodyMessage, style }: { headerMessage: string; bodyMessage: string; style?: React.CSSProperties }) {
   return (
     <Stack h="100%" style={style}>
       <Center h="100%">
