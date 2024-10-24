1730dcb41300043e7b83a406ed8de78852aa623f
Holger Stitz <holger.stitz@datavisyn.io>
2024-10-24 11:45:00 +0200

    fix: imports

--- src/vis/bar/SingleEChartsBarChart.tsx
+++ src/vis/bar/SingleEChartsBarChart.tsx
@@ -1,6 +1,4 @@
-import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Alert, Box, Stack, Text } from '@mantine/core';
+import { Box, Stack, Text } from '@mantine/core';
 import { useSetState } from '@mantine/hooks';
 import type { ScaleOrdinal } from 'd3v7';
 import type { BarSeriesOption } from 'echarts/charts';
@@ -10,6 +8,8 @@ import { type ECOption, useChart } from '../../echarts';
 import { useAsync } from '../../hooks';
 import { sanitize, selectionColorDark } from '../../utils';
 import { DEFAULT_COLOR, NAN_REPLACEMENT, SELECT_COLOR, VIS_NEUTRAL_COLOR, VIS_UNSELECTED_OPACITY } from '../general';
+import { ErrorMessage } from '../general/ErrorMessage';
+import { WarningMessage } from '../general/WarningMessage';
 import { ColumnInfo, EAggregateTypes, ICommonVisProps } from '../interfaces';
 import { useBarSortHelper } from './hooks';
 import { EBarDirection, EBarDisplayType, EBarGroupingType, EBarSortParameters, EBarSortState, IBarConfig, SortDirectionMap } from './interfaces';
@@ -23,9 +23,6 @@ import {
   sortSeries,
   WorkerWrapper,
 } from './interfaces/internal';
-import { BlurredOverlay } from '../../components';
-import { WarningMessage } from '../general/WarningMessage';
-import { ErrorMessage } from '../general/ErrorMessage';
 
 function generateHTMLString({ label, value, color }: { label: string; value: string; color?: string }): string {
   return `<div style="display: flex; gap: 8px">
