50943cf4f684f167a5805ec18dc2ef8710bbf2a3
Daniela <daniela.moitzi@datavisyn.io>
2024-10-29 16:30:27 +0100

    udpate sort icon story

--- src/vis/general/SortIcon.stories.tsx
+++ src/vis/general/SortIcon.stories.tsx
@@ -1,34 +1,86 @@
-import * as React from 'react';
-import type { Meta, StoryObj } from '@storybook/react';
-import { useArgs } from '@storybook/preview-api';
-// import { useArgs } from '@storybook/client-api';
+import React from 'react';
+import { ComponentStory } from '@storybook/react';
+import { Stack, Text, Group } from '@mantine/core';
+import { ESortStates, SortIcon, ISortIconProps } from './SortIcon';
 
-import { SortIcon, ESortStates } from './SortIcon';
+function Wrapper({ props, initialState }: { props?: Omit<ISortIconProps, 'sortState' | 'setSortState'>; initialState?: ESortStates }) {
+  const [sortState, setSortState] = React.useState<ESortStates>(initialState || ESortStates.NONE);
 
-const meta: Meta<typeof SortIcon> = {
-  component: SortIcon,
-  title: 'components/SortIcon',
-  decorators: [
-    function Component(Story, ctx) {
-      const [, setArgs] = useArgs();
+  return (
+    <Stack gap="xs">
+      <Group>
+        <SortIcon {...props} sortState={sortState} setSortState={setSortState} />
 
-      const setSortState = (value: ESortStates) => {
-        ctx.args.setSortState?.(value, false, {} as any);
+        {[2, 1, 5, 3, 4]
+          .sort((a, b) => {
+            switch (sortState) {
+              case ESortStates.ASC:
+                return a - b;
+              case ESortStates.DESC:
+                return b - a;
+              default:
+                return 0;
+            }
+          })
+          .map((i) => (
+            <Text key={i}>{i}</Text>
+          ))}
+      </Group>
+    </Stack>
+  );
+}
 
-        // Check if the component is controlled
-        if (ctx.args.sortState !== undefined) {
-          // Update the arg in Storybook
-          setArgs({ sortState: value });
-        }
-      };
+function Docs() {
+  return (
+    <Stack p="xl" gap="xl">
+      <Stack gap="xs">
+        <Text fz="xl" fw={500}>
+          Basic example
+        </Text>
+        <Wrapper />
+      </Stack>
+      <Stack gap="xs">
+        <Text fz="xl" fw={500}>
+          Compact icon
+        </Text>
+        <Wrapper props={{ compact: true }} />
+      </Stack>
+      <Stack gap="xs">
+        <Text fz="xl" fw={500}>
+          With priority
+        </Text>
+        <Wrapper props={{ priority: 1 }} />
+      </Stack>
+      <Stack gap="xs">
+        <Text fz="xl" fw={500}>
+          Has no unsorted state
+        </Text>
+        <Wrapper props={{ hasUnsortedState: false }} initialState={ESortStates.ASC} />
+      </Stack>
+      <Stack gap="xs">
+        <Stack gap={0}>
+          <Text fz="xl" fw={500}>
+            Sort descending on first click
+          </Text>
+          <Text c="dimmed">Only for specific use cases - the default is ascending</Text>
+        </Stack>
+        <Wrapper props={{ sortStateOnFirstClick: ESortStates.DESC }} />
+      </Stack>
+    </Stack>
+  );
+}
 
-      // Forward all args and overwrite setSortState
-      return <Story args={{ ...ctx.args, setSortState }} />;
-    },
-  ],
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Components/SortIcon',
+  component: Docs,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
 };
 
-export default meta;
-type Story = StoryObj<typeof SortIcon>;
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+// eslint-disable-next-line react/function-component-definition
+const Template: ComponentStory<typeof Docs> = (args) => {
+  return <Docs />;
+};
 
-export const Default: Story = {};
+export const Default: typeof Template = Template.bind({});
