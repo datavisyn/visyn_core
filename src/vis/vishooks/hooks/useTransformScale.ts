de2d36d92dbfdcb9cbe3936dcb930bdea1b8c836
Moritz Heckmann <moritz.heckmann@datavisyn.io>
2025-01-14 13:45:50 +0100

    Update useTransformScale

--- src/vis/vishooks/hooks/useTransformScale.ts
+++ src/vis/vishooks/hooks/useTransformScale.ts
@@ -1,34 +1,53 @@
 import { useMemo } from 'react';
-import { scaleLinear } from 'd3-scale';
+import { ZoomTransform as D3ZoomTransform, scaleLinear } from 'd3v7';
 import { ZoomTransform } from '../interfaces';
 import { rescaleX, rescaleY } from '../transform';
+import { sxi, txi, tyi } from '../math/matrix4x4';
 
 interface UseTransformScaleProps {
   domain: number[];
   range: number[];
   transform?: ZoomTransform;
   direction: 'x' | 'y';
+  transformTarget?: 'domain' | 'range';
 }
 
-export function useTransformScale({ domain, range, transform, direction }: UseTransformScaleProps) {
+export function useTransformScale({ domain, range, transform, direction, transformTarget }: UseTransformScaleProps) {
   return useMemo(() => {
+    // Invalid data case
     if (!domain || !range) {
       return null;
     }
 
     const scale = scaleLinear().domain(domain).range(range);
 
-    if (transform) {
+    // Untransformed case
+    if (!transform) {
       return {
         base: scale,
-        scaled: direction === 'x' ? rescaleX(transform, scale) : rescaleY(transform, scale),
+        scaled: scale,
       };
     }
 
-    return {
-      base: scale,
-      scaled: scale,
-    };
+    switch (transformTarget ?? 'domain') {
+      case 'domain':
+        return {
+          base: scale,
+          scaled: direction === 'x' ? rescaleX(transform, scale) : rescaleY(transform, scale),
+        };
+      case 'range': {
+        const d3transform = new D3ZoomTransform(transform[sxi]!, transform[txi]!, transform[tyi]!);
+        const transformedRange =
+          direction === 'x' ? scale.range().map((value) => d3transform.applyX(value)) : scale.range().map((value) => d3transform.applyY(value));
+
+        return {
+          base: scale,
+          scaled: scale.copy().range(transformedRange),
+        };
+      }
+      default:
+        throw new Error('Invalid transform target');
+    }
 
     // We dont want to compare with range/domain reference, only the primitive values
     // eslint-disable-next-line react-hooks/exhaustive-deps
