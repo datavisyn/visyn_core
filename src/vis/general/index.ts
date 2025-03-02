4e8590eb173e9448da89d8e35be053f4d2bd528c
Klaus Eckelt <klaus.eckelt@datavisyn.io>
2025-03-02 06:38:16 +0100

    feat: export vis LegendItem

--- src/vis/general/index.ts
+++ src/vis/general/index.ts
@@ -1,6 +1,7 @@
 // TODO: @dv-usama-ansari: We need to reorganize the exports in this file as well as the files within the `src/vis/general` directory.
 export * from './DownloadPlotButton';
 export * from './ErrorMessage';
+export * from './LegendItem';
 export * from './FastTextMeasure';
 export * from './SortIcon';
 export * from './WarningMessage';
