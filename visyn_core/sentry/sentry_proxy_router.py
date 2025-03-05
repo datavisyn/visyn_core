48b53fc94369b8bc0c388668c746df74bb9d5866
Michael Puehringer <michael.puehringer@datavisyn.io>
2025-03-05 17:08:09 +0100

    feat: consume ca_certs for Sentry frontend proxy

--- visyn_core/sentry/sentry_proxy_router.py
+++ visyn_core/sentry/sentry_proxy_router.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import ssl
 from urllib.parse import urlparse
 
 import httpx
@@ -16,7 +17,9 @@ _log = logging.getLogger(__name__)
 async def proxy_sentry_envelope(request: Request):  # pyright: ignore[reportUnusedFunction]
     dsn = manager.settings.visyn_core.sentry.get_frontend_dsn()
     if dsn:
-        async with httpx.AsyncClient(timeout=10) as client:
+        ca_certs = manager.settings.visyn_core.sentry.backend_init_options.get("ca_certs")
+        ssl_context = ssl.create_default_context(cafile=ca_certs) if ca_certs else manager.settings.visyn_core.sentry.frontend_proxy_verify
+        async with httpx.AsyncClient(timeout=manager.settings.visyn_core.sentry.frontend_proxy_timeout, verify=ssl_context) as client:
             # Example to parse a sentry envelope: https://github.com/getsentry/examples/blob/66da5f8c9559f64f1bfa57f8dd9b0731f75cd0e9/tunneling/python/app.py
             envelope = await request.body()
             piece = envelope.split(b"\n")[0].decode("utf-8")
