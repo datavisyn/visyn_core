5d343670de8e43f7f36ecef4f9afd880d8bd3ae5
Michael Puehringer <michael.puehringer@datavisyn.io>
2025-03-06 20:54:11 +0100

    feat: add Sentry demo configuration

--- src/demo/index.initialize.tsx
+++ src/demo/index.initialize.tsx
@@ -1,15 +1,29 @@
 import * as React from 'react';
 
+import * as Sentry from '@sentry/react';
 import { createRoot } from 'react-dom/client';
 
-import { MainApp } from './MainApp';
 import { VisynAppProvider } from '../app/VisynAppProvider';
+
+const MainApp = React.lazy(() => import('./MainApp').then((module) => ({ default: module.MainApp })));
+
+export const SENTRY_INIT_OPTIONS: Parameters<typeof VisynAppProvider>[0]['sentryInitOptions'] = {
+  integrations: (integrations) => [
+    ...integrations,
+    // Capture all console.error calls
+    Sentry.captureConsoleIntegration({ levels: ['error'] }),
+    // Instrument browser pageload/navigation performance
+    Sentry.browserTracingIntegration(),
+  ],
+};
 
 // create a new instance of the app
 createRoot(document.getElementById('main')!).render(
   <React.StrictMode>
-    <VisynAppProvider appName="Demo App" waitForClientConfig={false}>
-      <MainApp />
+    <VisynAppProvider appName="Demo App" waitForSentry sentryInitOptions={SENTRY_INIT_OPTIONS}>
+      <React.Suspense fallback={null}>
+        <MainApp />
+      </React.Suspense>
     </VisynAppProvider>
   </React.StrictMode>,
 );
