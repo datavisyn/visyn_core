c561de6db7cd32efd50df3caab67c125db488683
Holger Stitz <holger.stitz@datavisyn.io>
2025-03-28 11:34:51 +0100

    refactor: extract pure function

--- src/vis/VisTypeChooser.tsx
+++ src/vis/VisTypeChooser.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo } from 'react';
+import React, { useMemo } from 'react';
 
 import { Container, SimpleGrid } from '@mantine/core';
 import { useElementSize } from '@mantine/hooks';
@@ -6,23 +6,28 @@ import { useElementSize } from '@mantine/hooks';
 import { GeneralVis } from './Provider';
 import { VisTypeChooserCard } from './VisTypeChooserCard';
 
+/**
+ * Get the number of columns based on the width of the container.
+ * @param width container width
+ * @returns number of columns based on width
+ */
+function getCols(width: number): number {
+  if (width < 500) {
+    return 1;
+  }
+  if (width < 800) {
+    return 2;
+  }
+  if (width < 1400) {
+    return 3;
+  }
+  return 4;
+}
+
 export function VisTypeChooser({ visTypes, onClick }: { visTypes: GeneralVis[]; onClick?: (plotType: string) => void }) {
   const { ref, width } = useElementSize();
 
-  const getCols = useCallback((w: number): number => {
-    if (w < 500) {
-      return 1;
-    }
-    if (w < 800) {
-      return 2;
-    }
-    if (w < 1400) {
-      return 3;
-    }
-    return 4;
-  }, []);
-
-  const cols = useMemo(() => getCols(width), [width, getCols]);
+  const cols = useMemo(() => getCols(width), [width]);
 
   return (
     <Container fluid p="sm" h="95vh" pos="relative" w="100%" data-testid="vis-type-chooser">
