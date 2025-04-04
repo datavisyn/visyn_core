3aedfc477f415d8122b2e2c6d5449854dde8c535
Michael Pühringer <51900829+puehringer@users.noreply.github.com>
2025-04-04 11:59:27 +0200

    Remove size="sm" on anchor of login message

--- src/app/login/UserStoreUIMap.tsx
+++ src/app/login/UserStoreUIMap.tsx
@@ -124,7 +124,6 @@ export function AutoLoginForm({ setError, store }: IUserStoreRenderProps) {
           e.stopPropagation();
           login();
         }}
-        size="sm"
       >
         here
       </Anchor>{' '}
