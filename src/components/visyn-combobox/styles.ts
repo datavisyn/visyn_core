94e96145dd2f820053cc240d855f2763f97b1572
Moritz Heckmann <moritz.heckmann@datavisyn.io>
2025-05-19 11:06:18 +0200

    Combobox Options

---
+++ src/components/visyn-combobox/styles.ts
@@ -0,0 +1,26 @@
+import { css } from '@emotion/css';
+
+export const optionsDropdownCheckIcon = css`
+  .optionsDropdownCheckIcon {
+    opacity: 0.4;
+    width: 0.8em;
+    min-width: 0.8em;
+    height: 0.8em;
+
+    :where([data-combobox-selected]) & {
+      opacity: 1;
+    }
+  }
+`;
+
+export const optionsDropdownOptions = css`
+  .optionsDropdownOption {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+
+    &:where([data-reverse]) {
+      justify-content: space-between;
+    }
+  }
+`;
