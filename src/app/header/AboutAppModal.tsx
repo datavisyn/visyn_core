0e3d4dc3c7431f3a0e2ca828cf45e366204be399
Lizaveta Nekhviadovich <lizaveta.nekhviadovich@datavisyn.io>
2025-06-16 11:38:20 +0200

    style: make gap wider

--- src/app/header/AboutAppModal.tsx
+++ src/app/header/AboutAppModal.tsx
@@ -64,7 +64,7 @@ export function AboutAppModal({
       <Group my="md">{content}</Group>
       <Group gap="xs" wrap="nowrap">
         {VisynEnv.__VERSION__ ? (
-          <Group gap="xs" wrap="nowrap" my="xs">
+          <Group gap="xs" wrap="nowrap" my="md">
             <Text fw={700} c="dimmed">
               Version:
             </Text>
